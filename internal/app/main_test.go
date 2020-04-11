8dbaf216f5a770a9f284b7d3c28c4c94b91faa06
Blinnikov Andrei <goofinator@mail.ru>
2020-04-11 20:03:04 +0300

    test: test log on success and error

--- internal/app/main_test.go
+++ internal/app/main_test.go
@@ -2,6 +2,7 @@ package app_test
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -13,6 +14,7 @@ import (
 	"github.com/goofinator/hasher_nats_server/internal/init/startup"
 	"github.com/goofinator/hasher_nats_server/pkg"
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -35,31 +37,55 @@ var (
 	}
 )
 
-func TestProcessMessage(t *testing.T) {
+func TestProcessMessageSuccess(t *testing.T) {
+	//catch log
+	logBuf := &bytes.Buffer{}
+	log.SetOutput(logBuf)
+
 	//You need a NatsSession mock to process the request
 	mockController := gomock.NewController(t)
 	defer mockController.Finish()
 	ns := mocks.NewMockNatsSession(mockController)
 
-	setProcessMessageExpectations(ns, nil)
+	setProcessMessageExpectations(t, ns, nil)
 
 	ProcessMessage(ns, iniData, message)
+	assert.Empty(t, logBuf.String())
 }
 
-func setProcessMessageExpectations(ns *mocks.MockNatsSession, err error) {
+func TestProcessMessageFail(t *testing.T) {
+	//catch log
+	logBuf := &bytes.Buffer{}
+	log.SetOutput(logBuf)
+
+	//You need a NatsSession mock to process the request
+	mockController := gomock.NewController(t)
+	defer mockController.Finish()
+	ns := mocks.NewMockNatsSession(mockController)
+
+	setProcessMessageExpectations(t, ns, errors.New("some error"))
+
+	ProcessMessage(ns, iniData, message)
+	assert.Contains(t, logBuf.String(), string("error on SendMessage: some error\n"))
+}
+
+func setProcessMessageExpectations(t *testing.T, ns *mocks.MockNatsSession, err error) {
 	subject := fmt.Sprintf("worker.%s.in", clientUUID)
 
 	ns.EXPECT().
-		SendMessage(subject, NewMatcherMessage(message)).
+		SendMessage(subject, NewMatcherMessage(t, message)).
 		Return(err)
 }
 
-func NewMatcherMessage(msg *pkg.Message) gomock.Matcher {
+func NewMatcherMessage(t *testing.T, msg *pkg.Message) gomock.Matcher {
 	reply := &pkg.Message{
 		ID:   msg.ID,
 		Body: getTestBody(),
 	}
-	return &matcherMessage{msg: reply}
+	return &matcherMessage{
+		msg: reply,
+		t:   t,
+	}
 }
 
 // some fields of reply are randomly generated
@@ -67,21 +93,24 @@ func NewMatcherMessage(msg *pkg.Message) gomock.Matcher {
 // and should provide some matcher
 type matcherMessage struct {
 	msg *pkg.Message
+	t   *testing.T
 }
 
 func (m *matcherMessage) Matches(x interface{}) bool {
 	msg, ok := x.(*pkg.Message)
+	if !ok {
+		m.t.Errorf("fail to x.(*pkg.Message) in Matches")
+		return false
+	}
 	switch {
-	case !ok:
-		log.Fatal("fail to x.(*pkg.Message) in Matches")
 	case msg.Sender != serverUUID:
-		log.Println("msg.Sender != serverUUID")
+		m.t.Errorf("msg.Sender != serverUUID")
 	case msg.Type != pkg.DefaultMessageType:
-		log.Println("msg.Type != pkg.DefaultMessageType")
+		m.t.Errorf("msg.Type != pkg.DefaultMessageType")
 	case bytes.Compare(msg.Body, m.msg.Body) != 0:
-		log.Printf("msg body is not equal to specified in %q file", testDataFile)
+		m.t.Errorf("msg body is not equal to specified in %q file", testDataFile)
 	case msg.ID == m.msg.ID || msg.ID == clientUUID || msg.ID == serverUUID:
-		log.Println("msg.ID should differ from clientUUID, serverUUID, recieved message ID")
+		m.t.Errorf("msg.ID should differ from clientUUID, serverUUID, recieved message ID")
 	default:
 		return true
 	}
