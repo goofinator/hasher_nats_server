b7daf4348d0c9da41264158493b6f55f5fd99db9
Blinnikov Andrei <goofinator@mail.ru>
2020-04-11 17:59:14 +0300

    feat: calculate 100 different hashes

--- internal/utils/hash.go
+++ internal/utils/hash.go
@@ -1,6 +1,19 @@
 package utils
 
+import "crypto/sha256"
+
 // MakeHashes make hashes from message
 func MakeHashes(message []byte) [][]byte {
-	return [][]byte{[]byte("PLUG"), []byte("plug")}
+	result := make([][]byte, 0)
+	for i := byte(0); i < 100; i++ {
+		h := sha256.New()
+		h.Write(message)
+
+		// to make differen variants of hash
+		h.Write([]byte{i})
+
+		result = append(result, h.Sum(nil))
+	}
+
+	return result
 }
