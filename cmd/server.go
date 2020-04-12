77f92da8ee32b94d11bbd6a8bfca9ada79acd753
Blinnikov Andrei <goofinator@mail.ru>
2020-04-12 15:49:35 +0300

    refactor: cleanup function is obsolete

--- cmd/server.go
+++ cmd/server.go
@@ -4,8 +4,6 @@ import (
 	"github.com/goofinator/hasher_nats_server/internal/app"
 	"github.com/goofinator/hasher_nats_server/internal/init/startup"
 	"github.com/goofinator/hasher_nats_server/internal/web"
-	"github.com/goofinator/hasher_nats_server/pkg"
-	"github.com/nats-io/nats.go"
 )
 
 func main() {
@@ -14,10 +12,4 @@ func main() {
 	defer session.Close()
 
 	app.Process(session, iniData)
-}
-
-func cleanup(connection *nats.EncodedConn, ch chan<- *pkg.Message) {
-	connection.Drain()
-	connection.Close()
-	close(ch)
 }
